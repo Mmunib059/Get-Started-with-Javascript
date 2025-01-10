4948230ed2170d1618453c8c7561394424ce49bb
Muhammad Muneeb <80894553+Mmunib059@users.noreply.github.com>
2025-01-10 09:34:50 +0500

    callback function commit

---
+++ Funtion-and-methods/callbackfn.js
@@ -0,0 +1,15 @@
+// Synchronous Example:
+
+function greet(name, callback) {
+    console.log(`Hello, ${name}`);
+    callback();
+}
+
+function sayGoodbye() {
+    console.log("Goodbye!");
+}
+
+greet("Alice", sayGoodbye);
+// Output:
+// Hello, Alice
+// Goodbye!
